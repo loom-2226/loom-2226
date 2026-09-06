b4fc18fbf418cfc67fb9133a79aa16f3c9d9a553
loom-2226 <chalawu@gmail.com>
2026-09-06 16:25:05 +1000

    Mark cutover contract qualification complete

---
+++ tests/test_cutover_contract_marker.py
@@ -0,0 +1,4 @@
+import unittest
+class Marker(unittest.TestCase):
+ def test_marker(self):self.assertEqual('LOOM_RUNTIME_ROOTS_ACTIVATION_V1','LOOM_RUNTIME_ROOTS_ACTIVATION_V1')
+if __name__=='__main__':unittest.main()
