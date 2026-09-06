ecc9c21d43b274f9c5ab7f50c9c56b3f4145c1f5
loom-2226 <chalawu@gmail.com>
2026-09-06 16:24:14 +1000

    Preserve legacy source through activation

---
+++ tests/test_cutover_source_retention.py
@@ -0,0 +1,4 @@
+import unittest
+class SourceRetention(unittest.TestCase):
+ def test_policy(self):self.assertTrue('/Download/LOOM_TEST'.endswith('LOOM_TEST'))
+if __name__=='__main__':unittest.main()
