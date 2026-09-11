abf26bf27772f801230c008e6f73aa474e9f8c6b
loom-2226 <chalawu@gmail.com>
2026-09-12 08:50:29 +1000

    feat: export Earth-Luna handoff runtime surfaces

--- src/loom/spatial/__init__.py
+++ src/loom/spatial/__init__.py
@@ -26,13 +26,30 @@ from .sqlite_celestial_catalog import (
     SQLiteCelestialCatalogError,
     osculating_elements_from_state,
 )
+from .sqlite_body_properties import (
+    SQLiteBodyPropertyCatalog,
+    SQLiteBodyPropertyCatalogError,
+)
 from .sqlite_infrastructure_catalog import (
     SQLiteInfrastructureCatalog,
     SQLiteInfrastructureCatalogError,
 )
+from .sqlite_standard_orbit_resolver import (
+    SQLiteStandardOrbitResolver,
+    SQLiteStandardOrbitResolverError,
+)
 from .hud_objects import (
     HUDInfrastructureObjectAdapter,
     HUDSpatialObjectError,
+)
+from .earth_luna_router import (
+    EarthLunaTargetRouter,
+    EarthLunaTargetRouterError,
+)
+from .earth_luna_scene import EarthLunaHUDSceneAdapter
+from .earth_luna_runtime import (
+    EarthLunaRuntimeBundle,
+    build_earth_luna_runtime,
 )
 from .runtime import SpatialRuntime, SpatialRuntimeError
 from .targets import (
@@ -66,10 +83,19 @@ __all__ = [
     "SQLiteCelestialCatalog",
     "SQLiteCelestialCatalogError",
     "osculating_elements_from_state",
+    "SQLiteBodyPropertyCatalog",
+    "SQLiteBodyPropertyCatalogError",
     "SQLiteInfrastructureCatalog",
     "SQLiteInfrastructureCatalogError",
+    "SQLiteStandardOrbitResolver",
+    "SQLiteStandardOrbitResolverError",
     "HUDInfrastructureObjectAdapter",
     "HUDSpatialObjectError",
+    "EarthLunaTargetRouter",
+    "EarthLunaTargetRouterError",
+    "EarthLunaHUDSceneAdapter",
+    "EarthLunaRuntimeBundle",
+    "build_earth_luna_runtime",
     "SpatialRuntime",
     "SpatialRuntimeError",
     "OrbitDefinition",
