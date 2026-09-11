00ac3e8a83e69ead8580d41a2702d13f0221b657
loom-2226 <chalawu@gmail.com>
2026-09-12 08:13:56 +1000

    test: mark station detail scope explicitly deferred

--- tests/test_hud_spatial_objects.py
+++ tests/test_hud_spatial_objects.py
@@ -49,6 +49,7 @@ class HUDInfrastructureObjectTests(unittest.TestCase):
     def test_describe_ear_o01_exposes_thin_world_backed_hud_contract(self):
         obj = self.adapter.describe_object("EAR-O01")
         self.assertEqual(obj["contract"], "LOOM_HUD_SPATIAL_OBJECT_V1")
+        self.assertEqual(obj["detail_scope"], "THIN_WORLD_BACKED_V1")
         self.assertEqual(obj["entity_id"], "EAR-O01")
         self.assertEqual(obj["display_name"], "LEO Atlantic Exchange")
         self.assertEqual(obj["facility_type"], "ORBITAL_HABITAT_PORT")
